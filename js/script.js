2a686e282cd446ecba877f81114917c27d842e3d
Athul Krishna B <athul_krishna_b@Athuls-MacBook-Air.local>
2026-07-23 12:51:12 +0530

    fix mobile overflow

--- js/script.js
+++ js/script.js
@@ -34,3 +34,13 @@ AOS.init({
     easing: "ease-out",
     once: true
 });
+document.querySelector('.hero .btn').addEventListener('click', function(e) {
+    let ripple = document.createElement('span');
+    ripple.classList.add('ripple');
+    this.appendChild(ripple);
+    let x = e.clientX - e.target.offsetLeft;
+    let y = e.clientY - e.target.offsetTop;
+    ripple.style.left = x + 'px';
+    ripple.style.top = y + 'px';
+    setTimeout(() => ripple.remove(), 600);
+});
